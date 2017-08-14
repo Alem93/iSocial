0d5af6fd79cdffad28de1bdc1d05e8c632c04439
Alem93 <Alem93@users.noreply.github.com>
2017-08-14 10:56:25 -0300

    completed setting up Firebase integration

---
+++ iSocial/SignInViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iSocial
+//
+//  Created by Alejandro Mamchur on 8/14/17.
+//  Copyright © 2017 Alejandro Mamchur. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
