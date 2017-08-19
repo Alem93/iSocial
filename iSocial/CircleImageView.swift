350f4f966080ce4ce2448b78b78dedef756b73c3
Alem93 <Alem93@users.noreply.github.com>
2017-08-19 12:55:56 -0300

    Finish post wall view

--- iSocial/CircleImageView.swift
+++ iSocial/CircleImageView.swift
@@ -1,22 +1,28 @@
 //
-//  FancyView.swift
+//  CircleImageView.swift
 //  iSocial
 //
-//  Created by Alejandro Mamchur on 8/14/17.
+//  Created by Alejandro Mamchur on 8/19/17.
 //  Copyright © 2017 Alejandro Mamchur. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleImageView: UIImageView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 9.0
         layer.shadowOffset = CGSize(width: 1.0, height: 2.0)
         layer.cornerRadius = 2.0
+        
     }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+    }
+    
 }
