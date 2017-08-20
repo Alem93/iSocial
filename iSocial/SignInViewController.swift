99efa0e9946649fc764fbeadeab951b9bf187e34
Alem93 <Alem93@users.noreply.github.com>
2017-08-19 23:11:37 -0300

    finish user uid database

--- iSocial/SignInViewController.swift
+++ iSocial/SignInViewController.swift
@@ -53,7 +53,8 @@ class SignInViewController: UIViewController {
             } else {
                 print("Message: Successfully authenticated with Firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
             }
         })
@@ -65,7 +66,8 @@ class SignInViewController: UIViewController {
                 if error == nil {
                     print("Message: Email user authenticated with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                 } else {
                     FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: {(user, error) in
@@ -74,7 +76,8 @@ class SignInViewController: UIViewController {
                         } else {
                             print("Message: Succesfully authenticated with Firebase")
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
                             
                         }
@@ -84,7 +87,8 @@ class SignInViewController: UIViewController {
         }
     }
     
-    func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirebaseUser(uid: id, userData: userData)
         KeychainWrapper.defaultKeychainWrapper.set(id, forKey: KEY_UID)
         performSegue(withIdentifier: "goToFeed", sender: nil)
     }
